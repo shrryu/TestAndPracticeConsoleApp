af8d28c9ae9e959592dbd0d322e03f262c3b92e5
Hassan Hatamravesh <hhatamravesh@gmail.com>
2024-06-29 22:39:22 +0330

    Add project files.

---
+++ TestAndPracticeConsoleApp/RequiredMembers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAndPracticeConsoleApp
+{
+	class Asset
+	{
+		public required string Name { get; set; }
+	}
+
+	internal class RequiredMembers
+	{
+
+		public void RunCode()
+		{
+			//var ass = new Asset { Name = "vfjdo" };
+
+			/// this will hit error:
+			//var ass = new Asset();
+		}
+	}
+}
