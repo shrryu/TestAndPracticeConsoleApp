a9910498d42c1d59226218df8150bc881774610b
Hassan Hatamravesh <hhatamravesh@gmail.com>
2024-07-07 12:59:52 +0330

    Added RefLocals

---
+++ TestAndPracticeConsoleApp/RefLocals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAndPracticeConsoleApp
+{
+	internal class RefLocals
+	{
+		public void RunCode()
+		{
+			var array = new int[5] { 10, 20, 30, 40, 50 };
+
+			// this is a reference to the 4th element of array. so whenever this variable changes, the 4th element of array changes too.
+			ref int arrayElementReference = ref array[3];
+
+			arrayElementReference += 439;
+
+			for(int i = 0; i < 5; i++)
+			{
+				Console.WriteLine(array[i]);
+			}
+		}
+	}
+}
