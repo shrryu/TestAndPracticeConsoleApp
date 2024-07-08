2afce85c6e58996abe1008daaf1da3d4c6392346
Hassan Hatamravesh <hhatamravesh@gmail.com>
2024-07-08 18:29:30 +0330

    end of chapter 3

---
+++ TestAndPracticeConsoleApp/StaticInGeneric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAndPracticeConsoleApp
+{
+	public class Bob<T>
+	{
+        public static int Count { get; set; }
+    }
+	internal class StaticInGeneric
+	{
+		public void RunCode()
+		{
+			Console.WriteLine(++Bob<int>.Count);	// 1
+			Console.WriteLine(++Bob<int>.Count);	// 2
+			Console.WriteLine(++Bob<string>.Count); // 1
+			Console.WriteLine(++Bob<object>.Count); // 1
+		}
+	}
+}
